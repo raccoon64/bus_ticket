73ddd786368a5677cbf7a2f190f72d8e468e2f55
raccoon64 <wager.raccon@gmail.com>
2019-09-14 21:53:42 +0630

    first

---
+++ src/test/java/com/example/bus_tickets/BusTicketsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bus_tickets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BusTicketsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
